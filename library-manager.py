b8ea64eeef269bc4316b6885b7971c3e25c7d786
Aliza batool <alizzab2007@gmail.com>
2025-04-22 15:02:29 +0500

    added add a book funtionality

--- library-manager.py
+++ library-manager.py
@@ -5,9 +5,9 @@ st.set_page_config(layout="wide",page_icon="📔",page_title="Library-manager")
 st.title("LIBRARY MANAGER 📔")   
 
 library:list=[
-    {"title":"To Kill a Mockingbird","author":"	Harper Lee","genera":"Fiction / Classic","publication year":"1960"},
-    {"title":"1984","author":"	George Orwell","genera":"Dystopian / Political","publication year":"1949"},
-    {"title":"Harry Potter and the Sorcerer's Stone","author":"	J.K. Rowling","genera":"Fantasy / Young Adult","publication year":"1997"},
+    {"title":"To Kill a Mockingbird","author":"	Harper Lee","genera":"Fiction / Classic","publication_year":"1960","isread":True},
+    {"title":"1984","author":"	George Orwell","genera":"Dystopian / Political","publication_year":"1949","isread":True},
+    {"title":"Harry Potter and the Sorcerer's Stone","author":"	J.K. Rowling","genera":"Fantasy / Young Adult","publication_year":"1997","isread":False},
 ]
 
 book_cards= """
@@ -41,9 +41,45 @@ for book in library:
                 <h5>Title: {book["title"]}</h5>
                 <p>Author: {book["author"]}</p>
                 <p>Genera: {book["genera"]}</p>
-                <p>publication year: {book["publication year"]}</p>
+                <p>publication year: {book["publication_year"]}</p>
             </div>
        </div>
        
        """,unsafe_allow_html=True
     )
+st.sidebar.title("EDIT YOUR LIBRARY✏️")    
+st.sidebar.title("Scroll to see results of your selected options")    
+choices = st.sidebar.selectbox( "SELECT FROM THE FOLLOWING OPTIONS 📝 ", [
+        "Add a book",
+        "Remove a book",
+        "Display all books",
+        "Search for a book",
+        "Display statistics",])
+
+if choices == "Add a book":
+    
+    title = st.text_input("Enter books title")
+    author= st.text_input("Enter author name")
+    Genera = st.text_input("Enter Genera")
+    year = st.number_input("Enter books publication year")
+    isread = st.selectbox("Have you read this book",[True,False])
+    
+    
+    if st.button("add book"):
+       new_book = {
+                "title":title,
+                "author":author,
+                "genera":Genera,
+                "publication_year":year,
+                "isread":isread
+            }
+       library.append(new_book)
+       st.success(f"{title}✔️ written by {author}✏️ is added in to the library ✨✨")
+else:
+    print("You can not add a book")
+    
+if choices == "Remove a book":
+    st.title("REMOVE BOOKS")    
+    remove_by_title = st.text_input("Enter book title")
+    if st.button("Remove book"):
+        print("")
